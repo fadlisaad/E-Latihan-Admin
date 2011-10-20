4edc9f22d322a53420a2d91b018f6b3a210d9cd3
M.Fadli Saad <fadli@oddtech.com.my>
2011-10-20 18:13:28 +0800

    login

---
+++ login/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright 2009-2011 MARDI
+ * Developed by: Mohd Fadli Saad, znasystem@apadmedia.com, ZNA System Sdn Bhd
+ * This file is part of MARDILMS.
+ * The latest code can be found at http://elearn.mardi.gov.my/
+ * 
+ * Description : main script for configuration.
+ */
+	//Error state
+	error_reporting(0);					//currently set to 0 (for production), E_ALL (for development)
+	
+	//Database definition. Please change this to your database environment
+	define('DB_HOST', '/tmp/mysql/melanie.sock');		//Your hosting. 99% you don't need to change this
+    define('DB_USER', 'tiara');			//Your MySQL username
+    define('DB_PASSWORD', 'MIarNgYj');	//Your MySQL password
+    define('DB_DATABASE', 'melanie');	//Database that contain the application data
+?>

4b673c7cfe9b580bf048d1d6911bb6bacd7f1c53
M.Fadli Saad <fadli@oddtech.com.my>
2012-01-13 17:03:23 +0800

    php include: aside menu a.k.a main menu TODO: need to update and verify with client

---
+++ admin/aside.php
@@ -0,0 +1,29 @@
+<div id="aside" class="box">
+	<ul class="box">
+		<li id="submenu-active"><a href="#">Kursus &amp; Latihan</a>
+			<ul>
+				<li><a href="list-kategori.php">Senarai Kluster</a></li>
+				<li><a href="list-kursus.php">Senarai Kursus</a></li>
+				<li><a href="list-latihan.php">Senarai Latihan</a></li>
+				<li><a href="list.php">Senarai Penyelaras</a></li>
+			</ul>
+		</li>
+		<li id="submenu-active"><a href="#">Kewangan</a>
+			<ul>
+				<li><a href="list-bayaran.php">Honorarium</a></li>
+				<li><a href="list-bayaran-kursus.php">Penyata Perbelanjaan</a></li>
+				<li><a href="list-bayaran-pendahuluan.php">Bayaran Pendahuluan</a></li>
+				<li><a href="list-bayaran-invoice.php">Invois</a></li>
+			</ul>
+		</li>
+		<li id="submenu-active"><a href="#">Rekod Kursus</a>
+			<ul>
+				<li><a href="analisis-kursus.php">Penilaian Kursus</a></li>
+				<li><a href="analisis-penceramah.php">Penilaian Penceramah</a></li>
+				<li><a href="analisis-peserta.php">Analisis Peserta</a></li>
+				<li><a href="analisis-bayaran.php">Analisis Kluster</a></li>
+				<li><a href="analisis-bulanan.php">Analisis Bulanan</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>

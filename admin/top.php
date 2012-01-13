5f1d88c74a6cdeca93afd7a46fa4490e4078f8d1
M.Fadli Saad <fadli@oddtech.com.my>
2012-01-13 17:02:07 +0800

    php include: top menu

---
+++ admin/top.php
@@ -0,0 +1,8 @@
+<div id="menu" class="box">      
+	<ul class="box">
+		<li><div class="buttons"><a href="tambah-kursus.php"><img src="img/icons/add.png" alt=""/>Kursus</a></div></li>
+		<li><div class="buttons"><a href="tambah-lokasi.php"><img src="img/icons/add.png" alt=""/>Lokasi</a></div></li>
+		<li><div class="buttons"><a href="tambah-kategori.php"><img src="img/icons/add.png" alt=""/>Kluster</a></div></li>
+		<li><div class="buttons"><a href="tambah-admin.php"><img src="img/icons/add.png" alt=""/>Penyelaras</a></div></li>
+	</ul>
+</div>

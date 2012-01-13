a714b7a9df22871c7f0dbdf4bf135bbcfaf7a75e
M.Fadli Saad <fadli@oddtech.com.my>
2012-01-13 17:01:57 +0800

    php include: tray action

---
+++ admin/tray.php
@@ -0,0 +1,12 @@
+<div id="tray" class="box">
+	<p class="f-left box">
+	<!-- Switcher -->
+		<span class="f-left" id="switcher">
+		<a href="#" rel="1col" class="styleswitch ico-col1" title="Display one column"><img src="../design/switcher-1col.gif" alt="1 Column" /></a>
+		<a href="#" rel="2col" class="styleswitch ico-col2" title="Display two columns"><img src="../design/switcher-2col.gif" alt="2 Columns" /></a>
+		</span>
+		<span>E-Latihan: <strong>Program Kursus dan Latihan Teknikal</strong></span>
+	</p>
+	<p class="f-right">Pengguna: <?php echo $_SESSION['SESS_FULLNAME'] ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="../login/logout.php" id="logout">Log keluar</a></strong>
+	</p>
+</div>

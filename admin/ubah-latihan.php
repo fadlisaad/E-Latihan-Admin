cd3d8f346d2ea56a60302ddf0ce6b98d1d377e87
M.Fadli Saad <fadli@oddtech.com.my>
2012-01-13 17:04:34 +0800

    minified

--- admin/ubah-latihan.php
+++ admin/ubah-latihan.php
@@ -100,146 +100,50 @@ $row_kursus_lokasi = mysql_fetch_assoc($kursus_lokasi);
 $totalRows_kursus_lokasi = mysql_num_rows($kursus_lokasi);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><!-- InstanceBegin template="/Templates/Admin_Template.dwt" codeOutsideHTMLIsLocked="false" -->
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta http-equiv="content-language" content="en" />
-<meta name="robots" content="noindex,nofollow" />
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/reset.css" />
-<!-- RESET -->
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/main.css" />
-<!-- MAIN STYLE SHEET -->
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/2col.css" title="2col" />
-<!-- DEFAULT: 2 COLUMNS -->
-<link rel="alternate stylesheet" media="screen,projection" type="text/css" href="../css/1col.css" title="1col" />
-<!-- ALTERNATE: 1 COLUMN -->
-<!--[if lte IE 6]><link rel="stylesheet" media="screen,projection" type="text/css" href="css/main-ie6.css" /><![endif]-->
-<!-- MSIE6 -->
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/style.css" />
-<!-- GRAPHIC THEME -->
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/mystyle.css" />
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<?php include('meta.php'); ?>
 <link rel="stylesheet" media="screen,projection" type="text/css" href="../css/wysiwyg.css" />
-<!-- FAVICON -->
-<link href="http://elearn.mardi.gov.my/templates/jakyaniteii/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-<!-- DATEPICKER -->
-<link rel="stylesheet" type="text/css" href="../js/datepicker.css">
-<!-- JAVASCRIPTS -->
-<script type="text/javascript" src="../js/jquery.js"></script>
 <script type="text/javascript" src="../js/jquery-ui-1.7.2.custom.min.js"></script>
 <script type="text/javascript" src="../js/scripts.js"></script>
-<script type="text/javascript" src="../js/toggle.js"></script>
-<script type="text/javascript" src="../js/ui.core.js"></script>
-<script type="text/javascript" src="../js/ui.tabs.js"></script>
-<script type="text/javascript" src="../js/datepicker.js"></script>
-
-<!-- END OF HEADER -->
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Ubah Kursus</title>
-<!-- InstanceEndEditable --><!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable --><!-- InstanceParam name="sidebar" type="boolean" value="true" --><!-- InstanceParam name="footer" type="boolean" value="true" --><!-- InstanceParam name="header" type="boolean" value="true" -->
+<title>Ubah Latihan Amali</title>
 </head>
 
 <body>
 <div id="main">
 	<!-- Tray -->
-	<div id="tray" class="box">
-		<p class="f-left box">
-
-			<!-- Switcher -->
-			<span class="f-left" id="switcher">
-            <a href="#" rel="1col" class="styleswitch ico-col1" title="Display one column">
-            <img src="../design/switcher-1col.gif" alt="1 Column" />
-            </a><a href="#" rel="2col" class="styleswitch ico-col2" title="Display two columns">
-            <img src="../design/switcher-2col.gif" alt="2 Columns" /></a></span>
-
-			E-Latihan: <strong>Program Kursus dan Latihan Teknikal</strong></p>
-
-	  <p class="f-right">Pengguna: <?php echo $_SESSION['SESS_FULLNAME'] ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="../login/logout.php" id="logout">Log keluar</a></strong></p>
-
-  </div> <!--  /tray -->
-
+		<?php include('tray.php'); ?>
+	<!--  /tray -->
 	<hr class="noscreen" />
-
 	<!-- Menu -->
-	
-	<div id="menu" class="box">      
-      <ul class="box">
-        <li><div class="buttons"><a href="tambah-kursus.php"><img src="img/icons/add.png" alt=""/>Kursus</a></div></li>
-        <li><div class="buttons"><a href="tambah-lokasi.php"><img src="img/icons/add.png" alt=""/>Lokasi</a></div></li>
-        <li><div class="buttons"><a href="tambah-kategori.php"><img src="img/icons/add.png" alt=""/>Kluster</a></div></li>
-        <li><div class="buttons"><a href="tambah-admin.php"><img src="img/icons/add.png" alt=""/>Penyelaras</a></div></li>
-      </ul>
-    </div>
-
-	
+		<?php include('top.php'); ?>
 	<!-- /header -->
-
-<hr class="noscreen" />
-
+	<hr class="noscreen" />
 	<!-- Columns -->
 	<div id="cols" class="box">
-
-		<!-- Aside (Left Column) -->
-		
-		<div id="aside" class="box">
-          <ul class="box">
-          <li id="submenu-active"><a href="index.php">Halaman Utama</a></li>
-			<li id="submenu-active"><a href="#">Kursus</a>
-                <ul>
-                	<li><a href="list-kategori.php">Senarai Kluster</a></li>
-                  <li><a href="list-kursus.php">Senarai Kursus</a></li>
-                  <li><a href="list.php">Senarai Penyelaras</a></li>
-                  <li><a href="list-peserta.php">Senarai Peserta</a></li>
-                  <li><a href="list-penceramah.php">Senarai Penceramah</a></li>
-                  <li><a href="list-topik.php">Senarai Topik</a></li>
-                </ul>
-            </li>
-            <li id="submenu-active"><a href="#">Kewangan</a>
-                <ul>
-                	<li><a href="list-bayaran.php">Honorarium</a></li>
-                    <li><a href="list-bayaran-kursus.php">Penyata Perbelanjaan</a></li>
-                    <li><a href="list-bayaran-pendahuluan.php">Bayaran Pendahuluan</a></li>
-                    <li><a href="list-bayaran-invoice.php">Invois</a></li>
-                </ul>
-            </li>
-            <li id="submenu-active"><a href="#">Rekod Kursus</a>
-                <ul>
-                	<li><a href="analisis-kursus.php">Penilaian Kursus</a></li>
-                    <li><a href="analisis-penceramah.php">Penilaian Penceramah</a></li>
-                    <li><a href="analisis-peserta.php">Analisis Peserta</a></li>
-                    <li><a href="analisis-bayaran.php">Analisis Kluster</a></li>
-                    <li><a href="analisis-bulanan.php">Analisis Bulanan</a></li>
-                </ul>
-            </li>
-            </ul>
-	    </div>
-		
-		<!-- /aside -->
-
+	<!-- Aside (Left Column) -->
+		<?php include('aside.php'); ?>
+	<!-- /aside -->
     <hr class="noscreen" />
 
 		<!-- Content (Right Column) -->
 		<!-- InstanceBeginEditable name="content" -->
             <div id="content">
-              <h3>Ubah Kursus</h3>
+              <h3>Ubah Latihan Amali</h3>
               <form action="<?php echo $editFormAction; ?>" name="form" method="POST" id="form">
 			  <fieldset>
-                  <legend>Keterangan Kursus </legend>
+                  <legend>Keterangan Latihan Amali </legend>
                   <table width="100%" class="nostyle">
 					<tr>
 						<td width="15%">Tahun</td>
 						<td><input name="year" type="text" class="input-text" id="year" value="<?php echo $row_kursus_ubah['ts_latihan_year']; ?>" size="5" /></td>
 					</tr>
                     <tr>
-                      <td width="15%">Tajuk kursus</td>
+                      <td width="15%">Tajuk latihan amali</td>
                       <td><input name="kursus_nama" type="text" class="input-text" id="kursus_nama" value="<?php echo $row_kursus_ubah['ts_latihan_nama']; ?>" size="80" /></td>
                     </tr>
                     <tr>
-                      <td width="15%">Kod kursus</td>
+                      <td width="15%">Kod latihan amali</td>
                       <td><input name="kursus_kod" type="text" class="input-text" id="kursus_kod" value="<?php echo $row_kursus_ubah['ts_latihan_kod']; ?>" size="15" /></td>
-                    </tr>
-					<tr>
-                      <td width="15%">Tahun</td>
-                      <td><input name="year" type="text" class="input-text" id="year" value="<?php echo $row_kursus_ubah['ts_latihan_year']; ?>" size="10" /></td>
                     </tr>
                     <tr>
                       <td width="15%">Kluster</td>

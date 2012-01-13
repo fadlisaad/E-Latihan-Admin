f313768ca30c0a0cd589e93c8f8cdfd2b2a6fd0b
M.Fadli Saad <fadli@oddtech.com.my>
2012-01-13 17:02:34 +0800

    php include: meta information incl. js and css

---
+++ admin/meta.php
@@ -0,0 +1,23 @@
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="en" />
+<meta name="robots" content="noindex,nofollow" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/reset.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/main.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/2col.css" title="2col" />
+<link rel="alternate stylesheet" media="screen,projection" type="text/css" href="../css/1col.css" title="1col" />
+<!--[if lte IE 6]><link rel="stylesheet" media="screen,projection" type="text/css" href="css/main-ie6.css" /><![endif]-->
+<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/style.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/mystyle.css" />
+<link rel="stylesheet" type="text/css" href="../js/datepicker.css" />
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" src="../js/switcher.js"></script>
+<script type="text/javascript" src="../js/toggle.js"></script>
+<script type="text/javascript" src="../js/ui.core.js"></script>
+<script type="text/javascript" src="../js/ui.tabs.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".tabs > ul").tabs();
+	});
+</script>
+<script type="text/javascript" src="../js/datepicker.js"></script>

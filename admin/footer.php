bb3d5d77263c73207df5c63ea7bc32c26d5c192b
M.Fadli Saad <fadli@oddtech.com.my>
2012-01-13 17:02:55 +0800

    php include: footer

---
+++ admin/footer.php
@@ -0,0 +1,4 @@
+<div id="footer" class="box">
+	<p class="f-left"><span class="t-left">&copy; 2010 Hakcipta Terpelihara E-Latihan</span></p>
+	<p class="f-right"><span class="t-left">Pusat Kursus dan Latihan Teknikal, MARDI</span></p>
+</div>

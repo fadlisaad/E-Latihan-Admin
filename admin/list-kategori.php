66eae8af104793500271ee15badff22868943b32
M.Fadli Saad <fadli@oddtech.com.my>
2012-01-19 15:25:23 +0800

    minified as well

--- admin/list-kategori.php
+++ admin/list-kategori.php
@@ -28,132 +28,36 @@ $totalRows_sum_kategori = mysql_num_rows($sum_kategori);
 isset($startRow_add_kategori)? $orderNum=$startRow_add_kategori:$orderNum=0;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><!-- InstanceBegin template="/Templates/Admin_Template.dwt" codeOutsideHTMLIsLocked="false" -->
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta http-equiv="content-language" content="en" />
-<meta name="robots" content="noindex,nofollow" />
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/reset.css" />
-<!-- RESET -->
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/main.css" />
-<!-- MAIN STYLE SHEET -->
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/2col.css" title="2col" />
-<!-- DEFAULT: 2 COLUMNS -->
-<link rel="alternate stylesheet" media="screen,projection" type="text/css" href="../css/1col.css" title="1col" />
-<!-- ALTERNATE: 1 COLUMN -->
-<!--[if lte IE 6]><link rel="stylesheet" media="screen,projection" type="text/css" href="css/main-ie6.css" /><![endif]-->
-<!-- MSIE6 -->
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/style.css" />
-<!-- GRAPHIC THEME -->
-<link rel="stylesheet" media="screen,projection" type="text/css" href="../css/mystyle.css" />
-<!-- FAVICON -->
-<link href="http://elearn.mardi.gov.my/templates/jakyaniteii/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-<!-- JAVASCRIPTS -->
-<script type="text/javascript" src="../js/jquery.js"></script>
-<script type="text/javascript" src="../js/switcher.js"></script>
-<script type="text/javascript" src="../js/toggle.js"></script>
-<script type="text/javascript" src="../js/ui.core.js"></script>
-<script type="text/javascript" src="../js/ui.tabs.js"></script>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><?php include('meta.php'); ?>
+<?php include('meta.php'); ?>
 <script type="text/javascript" src="script/jquery.jeditable.mini.js"></script>
 <script type="text/javascript">
 jQuery(document).ready(function(){
 	$(".tabs > ul").tabs();
-		
 	$('.kategori').editable('save-kategori.php', {
-		 id			: 'id',
-		 submit 	: 'Save'
-	});	
+		 id		: 'id',
+		 submit : 'Save'
+	});
 });
 </script>
-<!-- END OF HEADER -->
-<!-- InstanceBeginEditable name="doctitle" -->
 <title>Senarai Kluster Kursus</title>
-<!-- InstanceEndEditable --><!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable --><!-- InstanceParam name="sidebar" type="boolean" value="true" --><!-- InstanceParam name="footer" type="boolean" value="true" --><!-- InstanceParam name="header" type="boolean" value="true" -->
 </head>
-
 <body>
 <div id="main">
 	<!-- Tray -->
-	<div id="tray" class="box">
-		<p class="f-left box">
-
-			<!-- Switcher -->
-			<span class="f-left" id="switcher">
-            <a href="#" rel="1col" class="styleswitch ico-col1" title="Display one column">
-            <img src="../design/switcher-1col.gif" alt="1 Column" />
-            </a><a href="#" rel="2col" class="styleswitch ico-col2" title="Display two columns">
-            <img src="../design/switcher-2col.gif" alt="2 Columns" /></a></span>
-
-			E-Latihan: <strong>Program Kursus dan Latihan Teknikal</strong></p>
-
-	  <p class="f-right">Pengguna: <?php echo $_SESSION['SESS_FULLNAME'] ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="../login/logout.php" id="logout">Log keluar</a></strong></p>
-
-  </div> <!--  /tray -->
-
+	<?php include('tray.php'); ?>
 	<hr class="noscreen" />
-
 	<!-- Menu -->
-	
-	<div id="menu" class="box">      
-      <ul class="box">
-        <li><div class="buttons"><a href="tambah-kursus.php"><img src="img/icons/add.png" alt=""/>Kursus</a></div></li>
-        <li><div class="buttons"><a href="tambah-lokasi.php"><img src="img/icons/add.png" alt=""/>Lokasi</a></div></li>
-        <li><div class="buttons"><a href="tambah-kategori.php"><img src="img/icons/add.png" alt=""/>Kluster</a></div></li>
-        <li><div class="buttons"><a href="tambah-admin.php"><img src="img/icons/add.png" alt=""/>Penyelaras</a></div></li>
-      </ul>
-    </div>
-
-	
-	<!-- /header -->
-
-<hr class="noscreen" />
-
+	<?php include('top.php'); ?>
+	<hr class="noscreen" />
 	<!-- Columns -->
 	<div id="cols" class="box">
-
-		<!-- Aside (Left Column) -->
-		
-		<div id="aside" class="box">
-          <ul class="box">
-          <li id="submenu-active"><a href="index.php">Halaman Utama</a></li>
-			<li id="submenu-active"><a href="#">Kursus</a>
-                <ul>
-                	<li><a href="list-kategori.php">Senarai Kluster</a></li>
-                  <li><a href="list-kursus.php">Senarai Kursus</a></li>
-                  <li><a href="list.php">Senarai Penyelaras</a></li>
-                  <li><a href="list-peserta.php">Senarai Peserta</a></li>
-                  <li><a href="list-penceramah.php">Senarai Penceramah</a></li>
-                  <li><a href="list-topik.php">Senarai Topik</a></li>
-                </ul>
-            </li>
-            <li id="submenu-active"><a href="#">Kewangan</a>
-                <ul>
-                	<li><a href="list-bayaran.php">Honorarium</a></li>
-                    <li><a href="list-bayaran-kursus.php">Penyata Perbelanjaan</a></li>
-                    <li><a href="list-bayaran-pendahuluan.php">Bayaran Pendahuluan</a></li>
-                    <li><a href="list-bayaran-invoice.php">Invois</a></li>
-                </ul>
-            </li>
-            <li id="submenu-active"><a href="#">Rekod Kursus</a>
-                <ul>
-                	<li><a href="analisis-kursus.php">Penilaian Kursus</a></li>
-                    <li><a href="analisis-penceramah.php">Penilaian Penceramah</a></li>
-                    <li><a href="analisis-peserta.php">Analisis Peserta</a></li>
-                    <li><a href="analisis-bayaran.php">Analisis Kluster</a></li>
-                    <li><a href="analisis-bulanan.php">Analisis Bulanan</a></li>
-                </ul>
-            </li>
-            </ul>
-	    </div>
-		
-		<!-- /aside -->
-
+	<!-- Aside (Left Column) -->
+	<?php include('aside.php'); ?>
     <hr class="noscreen" />
-
-		<!-- Content (Right Column) -->
-		<!-- InstanceBeginEditable name="content" -->
-            <div id="content">
-             <h3>Kluster Kursus </h3>
+	<!-- Content (Right Column) -->
+		<div id="content">
+             <h3>Kluster Kursus dan Latihan Amali</h3>
 			 <hr />
              <div class="buttons">
               <a href="tambah-kategori.php">
@@ -182,7 +86,7 @@ jQuery(document).ready(function(){
                   <?php } while ($row_add_kategori = mysql_fetch_assoc($add_kategori)); ?>
               </table>
 
-            </div><!-- InstanceEndEditable -->
+            </div>
 		<!-- /content -->
 	</div> 
 <!-- /cols -->
@@ -190,12 +94,7 @@ jQuery(document).ready(function(){
 	<hr class="noscreen" />
 
 	<!-- Footer -->
-	
-	<div id="footer" class="box">
-      <p class="f-left"><span class="t-left">&copy; 2010 Hakcipta Terpelihara E-Latihan</span></p>
-      <p class="f-right"><span class="t-left">Pusat Kursus dan Latihan Teknikal, MARDI</span></p>
-	  </div>
-  
+		<?php include('footer.php'); ?>
 	<!-- /footer -->
 </div> 
 <!-- /main -->

05bcf98a401af7e1d6ccba5f51f1a902cee90062
M.Fadli Saad <fadli@oddtech.com.my>
2012-02-09 18:01:15 +0800

    tambah laithan

--- admin/top.php
+++ admin/top.php
@@ -1,5 +1,6 @@
 <div id="menu" class="box">      
 	<ul class="box">
+		<li><div class="buttons"><a href="tambah-latihan.php"><img src="img/icons/add.png" alt=""/>Latihan</a></div></li>
 		<li><div class="buttons"><a href="tambah-kursus.php"><img src="img/icons/add.png" alt=""/>Kursus</a></div></li>
 		<li><div class="buttons"><a href="tambah-lokasi.php"><img src="img/icons/add.png" alt=""/>Lokasi</a></div></li>
 		<li><div class="buttons"><a href="tambah-kategori.php"><img src="img/icons/add.png" alt=""/>Kluster</a></div></li>
